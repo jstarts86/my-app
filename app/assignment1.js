bbcc5a2eea849d8be7ab1e7025e8e37ce8607348
John Song <jstarts86@gmail.com>
2023-11-28 19:28:46 +0900

    started assignment 1

---
+++ app/assignment1.js
@@ -0,0 +1,13 @@
+// pages/newpage.js
+import React from 'react';
+
+const NewPage = () => {
+    return (
+        <div>
+            <h1>This is a New Page</h1>
+            <p>Welcome to the new page!</p>
+        </div>
+    );
+};
+
+export default NewPage;
